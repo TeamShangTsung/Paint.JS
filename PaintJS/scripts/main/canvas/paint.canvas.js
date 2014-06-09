4f7cdbdf89e16f26cffecaa84a812bd24738edfc
luboganchev <luboganchev91@gmail.com>
2014-06-09 20:32:42 +0300

    Drawing functionality

--- PaintJS/scripts/main/canvas/paint.canvas.js
+++ PaintJS/scripts/main/canvas/paint.canvas.js
@@ -2,4 +2,38 @@
     paint.canvas = {};
     var me = paint.canvas;
 
+    var canvasElement, ctx;
+
+    $(function () {
+        canvasElement = $('canvas')[0];
+        ctx = canvasElement.getContext('2d');
+
+        var header = $('header')[0];
+        ctx.translate(0, -header.offsetHeight);
+    })
+
+    //Todo should be modified to base function
+    me.drawing = function () {
+        var isDrawing;
+
+        canvasElement.onmousedown = function (e) {
+            isDrawing = true;
+            ctx.moveTo(e.clientX, e.clientY);
+        };
+        canvasElement.onmousemove = function (e) {
+            if (isDrawing) {
+                ctx.lineTo(e.clientX, e.clientY);
+                ctx.stroke();
+            }
+        };
+        canvasElement.onmouseup = function () {
+            isDrawing = false;
+        };
+    }
+
+    me.stopDrawing = function () {
+        canvasElement.onmousedown = undefined;
+        canvasElement.onmousemove = undefined;
+        canvasElement.onmouseup = undefined;
+    }
 })(window.paint = window.paint || {}, jQuery);
