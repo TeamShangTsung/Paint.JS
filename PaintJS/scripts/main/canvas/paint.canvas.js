d106edfa2e48cde3b612189b9d678fd51ccd2236
luboganchev <luboganchev91@gmail.com>
2014-06-10 20:09:10 +0300

    Adding function for clearing the canvas

--- PaintJS/scripts/main/canvas/paint.canvas.js
+++ PaintJS/scripts/main/canvas/paint.canvas.js
@@ -65,6 +65,18 @@
         canvasElement.onmouseup = undefined;
     }
 
+    me.clearCanvas = function () {
+        // Store the current transformation matrix
+        ctx.save();
+
+        // Use the identity matrix while clearing the canvas
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+        // Restore the transform
+        ctx.restore();
+    }
+
     //Brush types
     var smoothingShadow = {
         init: function () {
