70627561d071bec4de5584beeaf85f47783bb39e
VDimitrova <vale_to_09@yahoo.com>
2014-06-15 14:35:26 +0300

    Draw Circle

--- PaintJS/scripts/main/shape/paint.shape.circle.js
+++ PaintJS/scripts/main/shape/paint.shape.circle.js
@@ -1,6 +1,34 @@
 ﻿(function (paint, $, undefined) {
-	paint.shape.circle = function () {
-		//paint.shape.call(this);
-		//implement base functions with this.
-	}
+    // Constructor
+    paint.shape.circle = function () {
+        // Call the parent constructor
+        paint.shape.call(this)
+        paint.canvasElement.addEventListener("mousedown", this.onMouseDown);
+
+        var self = this;
+        var ctx = paint.ctx;
+        var canvas = paint.canvasElement;
+
+        // override base methods
+        paint.shape.circle.prototype.onMouseMove = function (ev) {
+            // call base method
+            paint.shape.prototype.onMouseMove(ev);
+
+            ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+            var radius = Math.abs(sel.startPosition.x - self.finalPosition.x);
+            ctx.beginPath();
+            ctx.arc(self.startPosition.x, self.startPosition.y, radius, 0, 2 * Math.PI);
+            ctx.stroke();
+
+            canvas.addEventListener("mouseup", self.onMouseUp);
+        }
+    }
+
+    $(function () {
+        // inherit paint.shape
+        paint.shape.circle.prototype = new paint.shape();
+
+        // correct the constructor pointer because it points to paint.shape
+        paint.shape.circle.prototype.constructor = paint.shape.circle;
+    });
 })(window.paint = window.paint || {}, jQuery);
