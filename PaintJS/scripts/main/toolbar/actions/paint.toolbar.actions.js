33423073ce4a1242dfcd1a16feba7b7a20a5b627
Radoslav Chervenkov <chervenkovr@gmail.com>
2014-06-15 21:03:52 +0300

    some (temporary) toolbar functions

--- PaintJS/scripts/main/toolbar/actions/paint.toolbar.actions.js
+++ PaintJS/scripts/main/toolbar/actions/paint.toolbar.actions.js
@@ -12,6 +12,92 @@
         });
     };
 
+    me.spray = function(sprayWidth){
+        var sprayWidth = sprayWidth || 5;
+
+        paint.canvas.drawing({
+            type: 'spray',
+            lineWidth: sprayWidth
+        });
+    };
+
+    me.smoothingShadow = function(brushWidth, color){
+        var brushWidth = brushWidth || 1,
+            color = color || 'black';
+
+        paint.canvas.drawing({
+            type: 'smoothingShadow',
+            lineWidth: brushWidth,
+            color: color
+        });
+    };
+
+    me.radialGradient = function(brushWidth, color){
+        var brushWidth = brushWidth || 5,
+            color = color || 'black';
+
+        paint.canvas.drawing({
+            type: 'radialGradient',
+            lineWidth: brushWidth,
+            color: color
+        });
+    };
+
+    me.customPen = function(brushWidth, color){
+        var brushWidth = brushWidth || 5,
+            color = color || 'black';
+
+        paint.canvas.drawing({
+            type: 'customPen',
+            lineWidth: brushWidth,
+            color: color
+        });
+    };
+
+    me.slicedPen = function(brushWidth, color){
+        var brushWidth = brushWidth || 5,
+            color = color || 'black';
+
+        paint.canvas.drawing({
+            type: 'slicedPen',
+            lineWidth: brushWidth,
+            color: color
+        });
+    };
+
+    me.trailPen = function(brushWidth, color){
+        var brushWidth = brushWidth || 5,
+            color = color || 'black';
+
+        paint.canvas.drawing({
+            type: 'trailPen',
+            lineWidth: brushWidth,
+            color: color
+        });
+    };
+
+    me.randomRadius = function(brushRadius, color){
+        var brushRadius = brushRadius || 5,
+            color = color || 'black';
+
+        paint.canvas.drawing({
+            type: 'randomRadius',
+            lineWidth: brushRadius,
+            color: color
+        });
+    };
+
+    me.neighbourPoints = function(brushWidth, color){
+        var brushWidth = brushWidth || 5,
+            color = color || 'black';
+
+        paint.canvas.drawing({
+            type: 'neighbourPoints',
+            lineWidth: brushWidth,
+            color: color
+        });
+    };
+
     me.downloadDrawing = function (name) {
         var url = paint.canvasElement.toDataURL();
         name = name || "paintJS";
