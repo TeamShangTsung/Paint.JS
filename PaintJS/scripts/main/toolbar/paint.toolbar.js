0a9a6b7e38a3b3ff97eb1b5fae4762b622fa9394
adimitrov93 <a.dimitrov93@gmail.com>
2014-06-15 21:50:31 +0300

    Added almost full toolbar and some changes to paint.toolbar.js

--- PaintJS/scripts/main/toolbar/paint.toolbar.js
+++ PaintJS/scripts/main/toolbar/paint.toolbar.js
@@ -2,4 +2,25 @@
     paint.toolbar = {};
     var me = paint.toolbar;
 
+	$('#toolbar-container').on('click', '.tool', function(e) {
+		if (location.hash === ("#" + e.target.id)) {
+			location.hash = '';
+		}
+		else {
+			location.hash = ("#" + e.target.id);
+		}
+	});
+
+	$('#toolbar-container').on('click', '.brush-type', function(e) {
+		location.hash = ('#brush');
+	});
+
+	$('#toolbar-container').on('click', '#line-width-select', function(e) {
+		if (location.hash === '#line-width-select') {
+			location.hash = '';
+		}
+		else {
+			location.hash = '#line-width-select';
+		}
+	});
 })(window.paint = window.paint || {}, jQuery);
